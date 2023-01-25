8be17d7421d9276d430a2dddd3cf9b36489a2c41
Mr-Jester-Xiii <jestersilvester13@gmail.com>
2023-01-25 13:06:42 +0000

    Testing reset button for wiping generated grid

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let grid = document.querySelector(".grid");
 
-let columns = 128;
-let rows = 128;
+let columns = 10;
+let rows = 10;
 
-for (let i = 0; i < columns; ++i) {
-  let column = document.createElement("div");
-  column.className = "column";
-  for (let j = 0; j < rows; ++j) {
-    let row = document.createElement("div");
-    row.className = "row";
-    column.appendChild(row);
+// function changeColour() {
+//     alert("Touch");
+// }
+
+function drawGrid() {
+  for (let i = 0; i < columns; ++i) {
+    let column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < rows; ++j) {
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      column.appendChild(cell);
+    }
+    grid.appendChild(column);
   }
-  grid.appendChild(column);
 }
+
+function wipeGrid() {
+  while (grid.hasChildNodes())
+  grid.removeChild(grid.firstChild);
+}
+
+drawGrid();
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  wipeGrid();
+});
+
